97a659060a09e3e9b1d4f41dc45ede428b660036
Konstantine <Zeus_kosta@mail.ru>
2021-07-14 09:09:48 +0200

    The firss commit

---
+++ Js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//alert ("hallo you idiot"); 
+/* const result = confirm("Are you heare ?"); 
+console.log(result);  */
+/* const age = prompt("How old are you ?", ""); 
+console.log(age); 
+const Name = prompt(" Please enter your name",""); 
+console.log(Name);  */
+
+const answears =[];
+
+answears[0]= prompt(" What is your name?", ""); 
+answears[1]= prompt("What is your surname?"); 
+answears[2]= prompt("How old are you ?"); 
+
+console.log(answears); 
+console.log(typeof(answears)); 
+//document.write(answears); 
